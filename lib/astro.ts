b377833e8392a9ce30538079164ef7dccd26f332
Vojtěch Perník <develop@pervoj.cz>
2023-11-08 16:57:10 +0100

    Add button

--- lib/astro.ts
+++ lib/astro.ts
@@ -1,3 +1,4 @@
+export { default as Button } from "./button/Button.astro";
 export { default as Label } from "./label/Label.astro";
 export { default as textStyles } from "./label/styles";
 export { default as Luminar } from "./root/Root.astro";
