644e6581a0dee09dd46c7508c67fe0d741cbdae0
Vojtěch Perník <develop@pervoj.cz>
2024-03-31 21:17:35 +0200

    Add Luminar logo

---
+++ src/components/brand/LogoLuminar.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import LogoLuminar from "./LogoLuminar";
+
+const meta = {
+  title: "Brand/Luminar",
+  component: LogoLuminar,
+  args: { height: "3rem" },
+  render: ({ ...args }) => <LogoLuminar {...args} />,
+} satisfies Meta<typeof LogoLuminar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Logo: Story = {
+  name: "Logo",
+};
--- src/components/brand/index.ts
+++ src/components/brand/index.ts
@@ -1,3 +1,4 @@
+export * as LogoLuminar from "./LogoLuminar";
 export * as LogoMoductorFull from "./LogoMoductorFull";
 export * as LogoMoductorSymbol from "./LogoMoductorSymbol";
 export * as LogoMoductorType from "./LogoMoductorType";
