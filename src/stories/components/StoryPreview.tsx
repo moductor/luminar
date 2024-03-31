96ab5c52cc823d80b5df8d73780d43e2ea087caa
Vojtěch Perník <develop@pervoj.cz>
2024-03-31 21:14:20 +0200

    Update story preview component

--- src/stories/components/StoryPreview.tsx
+++ src/stories/components/StoryPreview.tsx
@@ -2,9 +2,20 @@ import { Canvas, Controls } from "@storybook/blocks";
 import { StoryObj } from "@storybook/react";
 import React from "react";
 
-export default function StoryPreview({ story }: { story: StoryObj }) {
+export default function StoryPreview({
+  title,
+  story,
+}: {
+  title?: string;
+  story: StoryObj;
+}) {
   return (
     <>
+      {title && (
+        <p role="heading" style={{ fontSize: "1rem", fontWeight: 700 }}>
+          {title}
+        </p>
+      )}
       <Canvas of={story}></Canvas>
       <Controls of={story} />
     </>
