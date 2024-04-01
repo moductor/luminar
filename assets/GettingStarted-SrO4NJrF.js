185f99f4c5ee9f6e2a9cc582d8c0cd6b70463dee
pervoj <pervoj@users.noreply.github.com>
2024-04-01 13:04:56 +0000

    deploy: ec1cd93be3d23699182b0e4d5cd028c61698cac2

---
+++ assets/GettingStarted-SrO4NJrF.js
@@ -0,0 +1,7 @@
+import{j as t}from"./jsx-runtime-BnIj46N_.js";import{useMDXComponents as o}from"./index-DLsXyOuj.js";import{M as i}from"./index-BDO2GMv1.js";import"./index-CsdIBAqE.js";import"./iframe-D8zMjpzd.js";import"../sb-preview/runtime.js";import"./index-CxKnHFOa.js";import"./index-5f0m1pej.js";import"./index-B_J8iUie.js";import"./index-B04q1QvB.js";import"./index-DrFu-skq.js";const s="0.0.3";function r(n){const e={code:"code",h1:"h1",p:"p",pre:"pre",...o(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Getting Started"}),`
+`,t.jsx(e.h1,{id:"getting-started",children:"Getting Started"}),`
+`,t.jsx(e.p,{children:"Luminar is not published to NPM and we don't plan to do it. However, you can install it using a tarball we generate for every release."}),`
+`,t.jsx(e.p,{children:"To install Luminar, run the command bellow:"}),`
+`,t.jsx(e.pre,{children:t.jsx(e.code,{children:`npm install --save https://github.com/moductor/luminar/releases/download/<VERSION>/release.tar.gz
+`})}),`
+`,t.jsxs(e.p,{children:["Don't forget to replace the text ",t.jsx(e.code,{children:"<VERSION>"})," with a version tag. ",t.jsxs("strong",{children:["Current version: ",t.jsxs("code",{children:["v",s]})]})]})]})}function f(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r(n)}export{f as default};
---
+++ assets/LogoMoductor-oXHHUIT5.js
@@ -0,0 +1,7 @@
+import{j as o}from"./jsx-runtime-BnIj46N_.js";import{useMDXComponents as c}from"./index-DLsXyOuj.js";import{M as i}from"./index-BDO2GMv1.js";import{L as s}from"./LogoPreview-N78W6Pps.js";import{L as m,a as f,b as l}from"./LogoMoductorType-DbOK_Ph7.js";import"./index-CsdIBAqE.js";import"./iframe-D8zMjpzd.js";import"../sb-preview/runtime.js";import"./index-CxKnHFOa.js";import"./index-5f0m1pej.js";import"./index-B_J8iUie.js";import"./index-B04q1QvB.js";import"./index-DrFu-skq.js";import"./index-DBqyYeau.js";function a(r){const t={h1:"h1",h2:"h2",p:"p",...c(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Brand/Moductor"}),`
+`,o.jsx(t.h1,{id:"moductor",children:"Moductor"}),`
+`,o.jsx(t.p,{children:"This is a documenation for Moductor branding."}),`
+`,o.jsx(t.h2,{id:"logo",children:"Logo"}),`
+`,o.jsx(s,{colorA:"#fafafa",colorB:"#39a7ff",render:({fg:n,style:e})=>o.jsx(m,{color:n,style:e})}),`
+`,o.jsx(s,{colorA:"#fafafa",colorB:"#39a7ff",render:({fg:n,style:e})=>o.jsx(f,{color:n,style:e})}),`
+`,o.jsx(s,{colorA:"#fafafa",colorB:"#39a7ff",render:({fg:n,style:e})=>o.jsx(l,{color:n,style:e})})]})}function D(r={}){const{wrapper:t}={...c(),...r.components};return t?o.jsx(t,{...r,children:o.jsx(a,{...r})}):a(r)}export{D as default};
